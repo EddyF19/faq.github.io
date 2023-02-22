cdde16e5c9e0662fcaeaea9d2ee85ff576f658b9
Peluche <eddyfrankely19@gmail.com>
2023-02-22 14:19:31 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Variable
+
+let accordion = document.getElementsByClassName('content-container');
+
+console.log(accordion);
+
+for( i = 0; i < accordion.length; i++ )
+{
+    accordion[i].addEventListener('click', function (){
+        this.classList.toggle('active')      
+    })
+}
